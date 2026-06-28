c1a59cc90cca288004ffd4e0a5d8b388ae387574
Viktor <klarkkent0314@gmail.com>
2026-06-28 18:11:52 +0300

    add Gunicorn

--- Dockerfile
+++ Dockerfile
@@ -22,3 +22,4 @@ COPY . .
 # entrypoint
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -20,5 +20,5 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput || echo "collectstatic failed, skipping"
 
-echo "Starting Django server..."
-python manage.py runserver 0.0.0.0:8000
+echo "Starting Gunicorn..."
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
